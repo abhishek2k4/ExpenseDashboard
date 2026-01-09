f3c909d440e329d5cf73d29f39a66a4cf1b34e93
abhishek2k4 <iabbyshek@gmail.com>
2026-01-09 23:18:33 +0530

    feat: implement TotalSpent component to calculate and display monthly expenses feat: add MonthHeader component as a placeholder for future enhancements

---
+++ src/components/MonthHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MonthHeader() {
+  return (
+    <div>MonthHeader</div>
+  )
+}
+
+export default MonthHeader
--- src/components/TotalSpent.jsx
+++ src/components/TotalSpent.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import data from "../data/transactions.json";
 
 function TotalSpent() {
+  const month = "2023-10";
+
+  const monthlyTotal = data
+    .filter((item) => item.date.startsWith(month))
+    .reduce((sum, item) => sum + item.amount, 0);
+
   return (
     <div className="bg-white p-2 rounded-xl">
       <center>
         <p className="font-bold text-sm text-gray-500">
           TOTAL SPENT THIS MONTH
-          <span className="text-2xl text-black mx-2">$8,000.35</span>
+          <span className="text-2xl text-black mx-2">
+             ₹{monthlyTotal.toFixed(2)}
+          </span>
         </p>
       </center>
     </div>
